ce12557850e5984fb56c5f1742b8e554e8cf7ca2
Arnel2 <arnelzuniga93@gmail.com>
2021-06-18 23:11:18 +0400

    Agregando administración

--- django_rest_api_crud/productos/admin.py
+++ django_rest_api_crud/productos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CategoriaProd,Producto
+
+
+class CategoriaProdAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "created", "updated")
+    readonly_fields = ("created", "updated")
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre","precio","disponibilidad", "created", "updated")
+    readonly_fields = ("created", "updated")
+
+
+admin.site.register(CategoriaProd, CategoriaProdAdmin)
+admin.site.register(Producto, ProductoAdmin)
